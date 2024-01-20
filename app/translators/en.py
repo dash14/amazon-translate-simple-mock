0953cbd8b4b958b447bd8e2b0b37b9894430a929
dash14 <dash14.ack@gmail.com>
2024-01-20 23:32:37 +0900

    Fix romanization process to keep original line breaks

--- app/translators/en.py
+++ app/translators/en.py
@@ -3,10 +3,15 @@ import pykakasi
 
 kks = pykakasi.kakasi()
 
-def to_romaji(text: str) -> str:
+def to_romaji_line(text: str) -> str:
     items = kks.convert(text)
     terms = list(map(lambda item: item['hepburn'], items))
     return ' '.join(terms)
+
+def to_romaji(text: str) -> str:
+    lines = text.split("\n")
+    items = [to_romaji_line(line) for line in lines]
+    return "\n".join(items)
 
 def en_translator(text: str, content_type: str) -> str:
     # Convert Japanese to Romaji
